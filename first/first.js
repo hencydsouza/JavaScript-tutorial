d8f83c842d06ed4fc04a5d91b2bb9d547f4cc1cc
Hency Dsouza <hencyhd39@gmail.com>
2024-02-08 22:41:11 +0530

    Conditional statements

--- first/first.js
+++ first/first.js
@@ -1,1 +1,11 @@
 console.log("Connected with external js file");
+
+var a = 1;
+var b = 2;
+
+if (a + b > 4) {
+    console.log(a + b)
+} else if (a + b < 1) {
+    console.log(a + b)
+} else
+    console.log("hi")
