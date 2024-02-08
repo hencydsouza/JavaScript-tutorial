20c7a674aaeea49c0141cdef18deb71ca1ba898e
Hency Dsouza <hencyhd39@gmail.com>
2024-02-08 22:55:24 +0530

    loops

--- first/first.js
+++ first/first.js
@@ -9,3 +9,34 @@ if (a + b > 4) {
     console.log(a + b)
 } else
     console.log("hi")
+
+for (var num = 0; num < 11; num += 2) {
+    console.log(num)
+}
+
+var a = [1, 2, 3, 4]
+
+a.forEach(item => console.log(item));
+
+for (var item of a) {
+    console.log(item);
+}
+
+var num = 0
+
+while (num < 5) {
+    console.log(num);
+    num++
+}
+
+do {
+    console.log(num)
+    num++
+} while (num < 5)
+
+var itr = 10
+console.log(itr++)
+console.log(itr)
+
+console.log(++itr)
+console.log(itr)
