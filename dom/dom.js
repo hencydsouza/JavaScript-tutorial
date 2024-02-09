ddd42bfcfb7c4bd2b34b1863fd157afd99a4b00b
Hency Dsouza <hencyhd39@gmail.com>
2024-02-09 10:37:48 +0530

    dom

---
+++ dom/dom.js
@@ -0,0 +1,30 @@
+console.log("JS file connected")
+// console.log(document)
+// console.dir(document)
+// console.log(document.domain)
+// console.log(document.URL)
+// console.log(document.body)
+// console.log(document.head)
+// console.log(document.title)
+// console.log(document.all)
+// console.log(document.all[4])
+
+// document.title = 12345
+// document.all[4].textContent = 'title'
+
+// var age = prompt("Please your enter age: ")
+
+// if (age > 20)
+//     alert("Great you are a valid user/visitor")
+// else
+//     alert("Great you are a invalid user/visitor")
+
+var head = document.getElementById("head1")
+
+console.log(head)
+
+var list_of_hobbies = document.getElementsByClassName("hobby")
+console.log(list_of_hobbies)
+
+var list_of_tags = document.querySelector('.hobby')
+console.log(list_of_tags)
