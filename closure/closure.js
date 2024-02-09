d687eaac6377d222f5ea8a6f15d6c4cd19be9c14
Hency Dsouza <hencyhd39@gmail.com>
2024-02-09 11:32:12 +0530

    currying

--- closure/closure.js
+++ closure/closure.js
@@ -21,3 +21,25 @@ function f1(gf) {
 }
 
 let f2 = (gf) => (f) => (s) => console.log(`${gf}-- ${f}-- ${s}`)
+
+// currying
+
+let add = function (x) {
+    return function (y) {
+        console.log(x + y)
+    }
+}
+
+let addby5 = add(5)
+
+console.log(addby5(2))
+console.log(addby5(5))
+console.log(addby5(10))
+
+let mul = function (x, y) {
+    console.log(x * y);
+}
+let mulBy10 = mul.bind(this,10);
+mulBy10(7);
+mulBy10(8);
+mulBy10(9);
