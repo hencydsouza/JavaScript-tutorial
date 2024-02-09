6f88882fc836ea9e0e2cb562124cec78f588370c
Hency Dsouza <hencyhd39@gmail.com>
2024-02-09 11:12:23 +0530

    dom

---
+++ events/events.js
@@ -0,0 +1,11 @@
+console.log('Connected')
+
+function change(id) {
+    id.innerHTML = 'CLICKED'
+}
+
+// var button = document.getElementById('btn')
+
+// button.addEventListener('click', function () {
+//     console.log('button was clicked')
+// })
