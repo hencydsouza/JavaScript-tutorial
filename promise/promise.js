4547103cd5b3e91344d03215a59ee7937add0767
Hency Dsouza <hencyhd39@gmail.com>
2024-02-09 11:55:51 +0530

    array functions

--- promise/promise.js
+++ promise/promise.js
@@ -8,3 +8,19 @@ const promise = new Promise((resolve, reject) => {
 })
 
 promise.then(result => result + " haha").catch(result => result + "hehe");
+
+// advanced array functions
+const add = (x, y) => x + y;
+
+add(2, 3);
+
+let arr = [10, 2, 3, 4, 30]
+let newarr = arr.map(num => num * 2)
+console.log(newarr)
+
+// filter
+let filarr = arr.filter(num => num > 9)
+console.log(filarr)
+
+// reduce
+let redarr = arr.reduce((acc, num) => acc + num, 100);
